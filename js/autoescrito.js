379c2019f562d403c21fff974a0704f3ecc7abdd
Elliam-Sanchez510 <elliamsanchez510@gmail.com>
2023-06-06 16:02:46 -0600

    texto autoescrito

---
+++ js/autoescrito.js
@@ -0,0 +1,16 @@
+var titulo = document.getElementById("autoescrito");
+var textoOriginal = titulo.innerHTML;
+var duracionAnimacion = 4; // Duración en segundos
+
+function borrarYReescribirTitulo() {
+  titulo.innerHTML = "";
+
+  for (var i = 0; i < textoOriginal.length; i++) {
+    setTimeout(function(index) {
+      titulo.innerHTML += textoOriginal[index];
+    }, i * (duracionAnimacion * 1000 / textoOriginal.length), i);
+  }
+}
+
+borrarYReescribirTitulo();
+setInterval(borrarYReescribirTitulo, duracionAnimacion * 1000 * 2);
