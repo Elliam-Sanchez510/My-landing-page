638ffacd4864329315bd56dcbfc1ea05df1a0313
Elliam-Sanchez510 <elliamsanchez510@gmail.com>
2023-06-10 11:34:36 -0600

    servicios

--- js/autoescrito.js
+++ js/autoescrito.js
@@ -1,12 +1,12 @@
 var titulo = document.getElementById("autoescrito");
 var textoOriginal = titulo.innerHTML;
-var duracionAnimacion = 4; // Duración en segundos
+var duracionAnimacion = 4;
 
 function borrarYReescribirTitulo() {
   titulo.innerHTML = "";
 
   for (var i = 0; i < textoOriginal.length; i++) {
-    setTimeout(function(index) {
+    setTimeout(function (index) {
       titulo.innerHTML += textoOriginal[index];
     }, i * (duracionAnimacion * 1000 / textoOriginal.length), i);
   }
